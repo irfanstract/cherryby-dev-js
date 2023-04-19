3a00565a5f6d1fac3e8b49dec02c96f57ea8db69
meh <meh@meh>
2023-04-20 00:17:47 +0700

    `packages\im-envited\src\App.tsx` - the Vite and React logo markup - expand into `<div style={{...}}>`

--- src/packages/im-envited/src/App.tsx
+++ src/packages/im-envited/src/App.tsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <div
+      style={{
+      }}
+      >
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
