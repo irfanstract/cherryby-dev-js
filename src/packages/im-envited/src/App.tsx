246e463e67688ff8790b539d2439a7232cb7bc75
meh <meh@meh>
2023-04-20 01:36:43 +0700

    `packages\im-envited\src\App.tsx` - `1 && import("cbtt") ;`

--- src/packages/im-envited/src/App.tsx
+++ src/packages/im-envited/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ToDoListComponent from 'src/projects/ToDoListComponent';
 import './App.css'
+1 && import("cbtt") ;
 
 function App() {
   const [count, setCount] = useState(0)
