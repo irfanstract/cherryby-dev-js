d9a0d70507ad636dd03b79a8cb1057afb72fbbef
meh <meh@meh>
2023-04-19 23:15:07 +0700

    introduce `packages\im-envited\src\window-polyfills.tsx`

---
+++ src/packages/im-envited/src/window-polyfills.tsx
@@ -0,0 +1,32 @@
+/** 
+ * init/setup the polyfills for `global`
+ * 
+ */
+
+
+
+
+import "core-js/actual" ;
+// import "core-js/full" ;
+import "core-js/proposals/map-upsert-stage-2" ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {} ; // TS(1208)
+
