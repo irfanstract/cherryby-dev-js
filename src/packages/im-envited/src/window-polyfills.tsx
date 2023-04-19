9c7f5cdcb64d67ca769b77d5e4cfa16cf8a8d78e
meh <meh@meh>
2023-04-19 23:23:46 +0700

    `packages\im-envited\src\window-polyfills.tsx` - add `getYyyCTM` polyfill
    ```
     * for quite a while
     * these two methods of {@link SVGGraphicsElement }
     * were initially specified to return the SVG-specific `interface`s, so
     * it'd take a while until such change gets properly implemented by browsers
    ```

--- src/packages/im-envited/src/window-polyfills.tsx
+++ src/packages/im-envited/src/window-polyfills.tsx
@@ -18,6 +18,39 @@ import "core-js/proposals/map-upsert-stage-2" ;
 
 
 
+/** 
+ * for quite a while 
+ * these two methods of {@link SVGGraphicsElement } were initially specified to return the SVG-specific `interface`s, so
+ * it'd take a while until such change gets properly implemented by browsers
+ * 
+ */
+for (const methodName of (
+  ["getScreenCTM", "getCTM"] satisfies 
+  (keyof Pick<SVGGraphicsElement, "getCTM" | "getScreenCTM">)[]
+)) {
+  const originalImpl = (
+    SVGGraphicsElement.prototype[methodName]
+  ) ;
+  SVGGraphicsElement.prototype[methodName] = (
+    function (this: SVGGraphicsElement, ...args ) {
+      const originalReturnVal = (
+        originalImpl.apply(this, ...args) as 
+        (SVGMatrix | DOMMatrix )
+      ) ;
+      const newReturnVal = (
+        // TODO
+        DOMMatrix.fromMatrix((
+          originalReturnVal
+        ))
+      ) satisfies DOMMatrix ;
+      return (
+        newReturnVal
+      ) ;
+    }
+  ) ;
+} ;
+
+
 
 
 
