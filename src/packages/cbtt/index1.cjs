6cc066d953068ccc4d5d57e4dbc1d7cbfeeb263b
meh <meh@meh>
2023-04-19 14:42:14 +0700

    `packages\cbtt\` - introduce `index1.cjs`

---
+++ src/packages/cbtt/index1.cjs
@@ -0,0 +1,28 @@
+"use strict" ;
+
+/** 
+ * https://typestrong.org/ts-node/docs/usage/#node-flags-and-other-tools 
+ * 
+ */
+if ((
+  require("cbtt/is-browser-or--nodejs.cjs")
+  .isNode
+) ) {
+  console["log"](`[CBTT]`, `detected Node env`) ;
+  console["log"](`[CBTT]`, `prerequisite - 'ts-node/register'`) ;
+  require("ts-node/register") ;
+  console["log"](`[CBTT]`, `done`) ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
