55f07b6dd679941e480a07baf0779d35cd1cc88e
meh <meh@meh>
2023-04-21 08:20:28 +0700

    `cbtt/xmf` - introduce `util.ts`

---
+++ src/packages/cbtt/src/xmf/util.ts
@@ -0,0 +1,30 @@
+
+
+/* 
+ * until the relevant spec adds *spread-exports*,
+ * will need to have *spread-exports* expressed as *static wildcard-exports* here at top
+ */
+export * from "cbtt/src/util/all" ;
+
+
+
+
+
+
+
+
+
+
+import * as util from "cbtt/src/util/all" ;
+
+
+
+
+
+
+
+
+
+
+
+export {} ; // TS(1208)
