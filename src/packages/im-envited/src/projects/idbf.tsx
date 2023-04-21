6f2896e71e6c4107a3b9cd30a8d2545ccd493898
meh <meh@meh>
2023-04-22 00:45:48 +0700

    introduce `src\packages\im-envited\src\projects\idbf.tsx`

---
+++ src/packages/im-envited/src/projects/idbf.tsx
@@ -0,0 +1,33 @@
+/** 
+ * 
+ * @deprecated
+ */
+
+import * as util from "im-envited/src/projects/utility-functions/all" ;
+
+
+
+
+// import 
+// import * as pm from "cbtt/src/ems-fs-native.cjs" ;
+// const { FS: files, IDBFS, } = (pm as Partial<typeof global> ) ;
+import "cbtt/src/emae" ;
+
+
+
+export default () => {
+  return (
+    <div>
+      <p>IDBF</p>
+    </div>
+  ) ;
+} ;
+
+
+
+
+
+
+
+
+export {} ; // TS(1208)
