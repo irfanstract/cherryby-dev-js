4261774d15f5737cfc7e5a9790cd3236274e43b1
meh <meh@meh>
2023-04-21 20:43:49 +0700

    `im-envited\src\main.tsx` - add boundary `<WithErrorBound>`

--- src/packages/im-envited/src/main.tsx
+++ src/packages/im-envited/src/main.tsx
@@ -19,9 +19,11 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <WithErrorBound>
     <IonApp>
     <App />
     </IonApp>
+    </WithErrorBound>
   </React.StrictMode>,
 )
 
