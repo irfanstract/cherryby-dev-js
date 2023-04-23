dbef75ef50cf6bd4564af475a521a880d5af6942
meh <meh@meh>
2023-04-23 23:43:16 +0700

    `im-envited\src\main.tsx` - `import "im-envited/src/setupIonicReact1" ;`

--- src/packages/im-envited/src/main.tsx
+++ src/packages/im-envited/src/main.tsx
@@ -5,6 +5,7 @@ import "im-envited/src/window-polyfills"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import WithErrorBound from "src/projects/JsxErrorBound";
+import "im-envited/src/setupIonicReact1" ;
 import { IonApp, } from "@ionic/react"
 import { IonContent, } from "@ionic/react"
 import App from './App'
