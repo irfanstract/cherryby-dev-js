96b32a99ff8518464f7174a40a1682a23854fa2d
meh <meh@meh>
2023-04-23 23:58:16 +0700

    `im-envited\src\main.tsx` - `import { IonContent, } from "@ionic/react"`

--- src/packages/im-envited/src/main.tsx
+++ src/packages/im-envited/src/main.tsx
@@ -5,6 +5,7 @@ import "im-envited/src/window-polyfills"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { IonApp, } from "@ionic/react"
+import { IonContent, } from "@ionic/react"
 import App from './App'
 import './index.css'
 
